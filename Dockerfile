db8d1e5ebef63b4a4b1bed8bba21def303fad38f
ymmtmdk <ymmtmdk@gmail.com>
2017-04-02 23:55:56 +0000

    Update

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ WORKDIR mkdir $HOME/setup_scripts
 RUN apt-get update
 RUN apt-get install -y apt-utils
 RUN apt-get install -y sudo
-COPY setup_*.sh Dockerfile ./
+COPY setup_admin.sh ./
 RUN sh setup_admin.sh
+COPY setup_user.sh fisher.rb ./
 RUN sh setup_user.sh
--- setup_user.sh
+++ setup_user.sh
@@ -1,14 +1,13 @@
 git clone https://github.com/ymmtmdk/dotfiles $HOME/dotfiles
+mkdir -p $HOME/.config/fish
 ruby $HOME/dotfiles/link.rb
 
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_install.sh
 sh dein_install.sh $HOME/.config/nvim/dein
 pip install --user neovim percol
-nvim -c "q"
-nvim -c "call dein#update() | q"
+nvim -c --headless "q" > /dev/null
 
 echo "" > $HOME/dotfiles/fish/each_arch.fish
-mkdir -p $HOME/.config/fish
 curl -Lo $HOME/.config/fish/functions/fisher.fish --create-dirs git.io/fisherman
 ruby fisher.rb $HOME/dotfiles/fish/fishfile
 
