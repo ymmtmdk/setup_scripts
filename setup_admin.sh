928ef7edbfbdd833e3860b5e085444f11c57ac13
ymmtmdk <ymmtmdk@gmail.com>
2017-04-10 02:10:48 +0000

    Update

--- setup_admin.sh
+++ setup_admin.sh
@@ -1,22 +1,25 @@
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository -y ppa:brightbox/ruby-ng
-sudo add-apt-repository -y ppa:neovim-ppa/stable
-sudo apt-add-repository -y ppa:fish-shell/release-2
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates
-sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
-sudo apt-get install -y git curl
-sudo apt-get install -y fish
-sudo apt-get install -y ruby
-sudo apt-get install -y python-dev python-pip python3-dev python3-pip
-sudo apt-get install -y neovim
-sudo apt-get install -y aptitude
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:brightbox/ruby-ng
+add-apt-repository -y ppa:neovim-ppa/stable
+apt-add-repository -y ppa:fish-shell/release-2
+apt-get update
+apt-get install -y apt-transport-https ca-certificates
+apt-get install -y libssl-dev libreadline-dev zlib1g-dev
+apt-get install -y git curl
+apt-get install -y fish
+apt-get install -y ruby
+apt-get install -y python3-dev python3-pip
+apt-get install -y neovim
+apt-get install -y aptitude
 
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
 
-sudo apt-get install -y docker-ce
+apt-get install -y docker-ce
 
-sudo pip install docker-compose
+pip3 install docker-compose
+
+mkdir -p /usr/local/etc/profile.d/
+curl https://raw.githubusercontent.com/rupa/z/master/z.sh > /usr/local/etc/profile.d/z.sh
 
--- setup_user.sh
+++ setup_user.sh
@@ -2,12 +2,12 @@ git clone https://github.com/ymmtmdk/dotfiles $HOME/dotfiles
 mkdir -p $HOME/.config/fish
 ruby $HOME/dotfiles/link.rb
 
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_install.sh
-sh dein_install.sh $HOME/.config/nvim/dein
-pip install --user neovim percol
-nvim -c --headless "q" > /dev/null
-
 echo "" > $HOME/dotfiles/fish/each_arch.fish
 curl -Lo $HOME/.config/fish/functions/fisher.fish --create-dirs git.io/fisherman
 ruby fisher.rb $HOME/dotfiles/fish/fishfile
 
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_install.sh
+sh dein_install.sh $HOME/.config/nvim/dein
+pip3 install --user neovim percol
+nvim -c "call dein#update()" -c "q!"
+reset
