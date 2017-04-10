5a3d5c015d449a83a11183fece83cbb38c0ea734
ymmtmdk <ymmtmdk@gmail.com>
2017-04-10 02:40:24 +0000

    Update

--- setup_user.sh
+++ setup_user.sh
@@ -6,8 +6,15 @@ echo "" > $HOME/dotfiles/fish/each_arch.fish
 curl -Lo $HOME/.config/fish/functions/fisher.fish --create-dirs git.io/fisherman
 ruby fisher.rb $HOME/dotfiles/fish/fishfile
 
+curl -LO https://github.com/peco/peco/releases/download/v0.5.1/peco_linux_386.tar.gz
+mkdir $HOME/mybin
+tar xvf peco_linux_386.tar.gz
+cp peco_linux_386/peco $HOME/mybin
+
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_install.sh
 sh dein_install.sh $HOME/.config/nvim/dein
-pip3 install --user neovim percol
+
+pip3 install --user neovim
 nvim -c "call dein#update()" -c "q!"
 reset
+echo ok
