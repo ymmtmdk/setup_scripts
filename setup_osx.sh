d163d68ee0ebe467d074e70066bb0070fe40a4ab
ymmtmdk <ymmtmdk@gmail.com>
2017-04-29 20:06:28 +0900

    Update setup_osx.sh

--- setup_osx.sh
+++ setup_osx.sh
@@ -1,20 +1,31 @@
+sudo echo start
+
+brew tap caskroom/cask
+brew tap neovim/neovim
+brew cask install dropbox karabiner iterm2
+
+brew install reattach-to-user-namespace rbenv readline zlib fish neovim python3
+
+rbenv install 2.4.1
+pip3 install --user neovim
+
 git clone https://github.com/ymmtmdk/dotfiles $HOME/dotfiles
 mkdir -p $HOME/.config/fish
 ruby $HOME/dotfiles/link.rb
 
-echo "" > $HOME/dotfiles/fish/each_arch.fish
+# echo "" > $HOME/dotfiles/fish/each_arch.fish
 curl -Lo $HOME/.config/fish/functions/fisher.fish --create-dirs git.io/fisherman
-ruby fisher.rb $HOME/dotfiles/fish/fishfile
+#ruby fisher.rb $HOME/dotfiles/fish/fishfile
+fish -c 'fisher'
 
-curl -LO https://github.com/peco/peco/releases/download/v0.5.1/peco_linux_386.tar.gz
+#curl -LO https://github.com/peco/peco/releases/download/v0.5.1/peco_linux_386.tar.gz
 mkdir $HOME/mybin
-tar xvf peco_linux_386.tar.gz
-cp peco_linux_386/peco $HOME/mybin
+#tar xvf peco_linux_386.tar.gz
+#cp peco_linux_386/peco $HOME/mybin
 
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_install.sh
 sh dein_install.sh $HOME/.config/nvim/dein
 
-pip3 install --user neovim
 nvim -c "call dein#update()" -c "q!"
 reset
 echo ok
