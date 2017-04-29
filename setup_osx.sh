02c1984ff5d892d8d030941681f0a8e1dcd08c5b
ymmtmdk <ymmtmdk@gmail.com>
2017-04-29 20:39:41 +0900

    Update setup_osx.sh

--- setup_osx.sh
+++ setup_osx.sh
@@ -17,11 +17,6 @@ git clone https://github.com/ymmtmdk/dotfiles $HOME/dotfiles
 mkdir -p $HOME/.config/fish
 ruby $HOME/dotfiles/link.rb
 
-# echo "" > $HOME/dotfiles/fish/each_arch.fish
-curl -Lo $HOME/.config/fish/functions/fisher.fish --create-dirs git.io/fisherman
-#ruby fisher.rb $HOME/dotfiles/fish/fishfile
-fish -c 'fisher'
-
 #curl -LO https://github.com/peco/peco/releases/download/v0.5.1/peco_linux_386.tar.gz
 mkdir $HOME/mybin
 #tar xvf peco_linux_386.tar.gz
@@ -31,5 +26,11 @@ curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh >
 sh dein_install.sh $HOME/.config/nvim/dein
 
 nvim -c "call dein#update()" -c "q!"
+
+# echo "" > $HOME/dotfiles/fish/each_arch.fish
+curl -Lo $HOME/.config/fish/functions/fisher.fish --create-dirs git.io/fisherman
+#ruby fisher.rb $HOME/dotfiles/fish/fishfile
+fish -c 'fisher'
+
 reset
 echo ok
