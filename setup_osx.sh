6b17f5e1caebd145356cc16966409c7efb7c7aff
ymmtmdk <ymmtmdk@gmail.com>
2017-04-29 21:14:52 +0900

    Update setup_osx.sh

--- setup_osx.sh
+++ setup_osx.sh
@@ -8,7 +8,7 @@ brew tap caskroom/cask
 brew tap neovim/neovim
 brew cask install dropbox karabiner iterm2
 
-brew install reattach-to-user-namespace rbenv readline zlib fish neovim python3
+brew install reattach-to-user-namespace rbenv readline zlib fish neovim python3 peco
 
 rbenv install 2.4.1
 pip3 install --user neovim
